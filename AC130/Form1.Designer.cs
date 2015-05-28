657b4cf0db175414292ee6324925558cdf16a79e
Elliott Wang <elliott.wang@hologic.com>
2015-05-28 11:38:33 -0700

    Initial AC130 commit

---
+++ AC130/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace AC130
+{
+    partial class AC130
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lstate = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelDelay = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lstate
+            // 
+            this.lstate.AutoSize = true;
+            this.lstate.Location = new System.Drawing.Point(66, 29);
+            this.lstate.Name = "lstate";
+            this.lstate.Size = new System.Drawing.Size(21, 13);
+            this.lstate.TabIndex = 1;
+            this.lstate.Text = "Off";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // labelDelay
+            // 
+            this.labelDelay.AutoSize = true;
+            this.labelDelay.Location = new System.Drawing.Point(12, 9);
+            this.labelDelay.Name = "labelDelay";
+            this.labelDelay.Size = new System.Drawing.Size(37, 13);
+            this.labelDelay.TabIndex = 5;
+            this.labelDelay.Text = "Delay:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Status:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Counter:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "X1, Y1:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "X2, Y2:";
+            // 
+            // AC130
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(194, 134);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelDelay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstate);
+            this.Controls.Add(this.textBox1);
+            this.Name = "AC130";
+            this.Text = "AC130";
+            this.Load += new System.EventHandler(this.AC130_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lstate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelDelay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ AC130/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AC130
+{
+    public partial class AC130 : Form
+    {
+        static Thread AutoClick;
+        static Thread AutoClick2;
+        static int militime;
+        static int counter = 0;
+        public static int WM_HOTKEY = 0x312;
+        private int x1 = 0;
+        private int y1 = 0;
+        private int x2 = 0;
+        private int y2 = 0;
+
+        [DllImport("user32.dll")]
+        public static extern bool SetCursorPos(int x, int y);
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public void AClick()
+        {
+            while (true)
+            {
+                int x = Cursor.Position.X;
+                int y = Cursor.Position.Y;
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+                counter++;
+                label1.Text = counter.ToString();
+                Thread.Sleep(militime);
+            }
+        }
+
+        public void AClick2()
+        {
+            while (true)
+            {
+                SetCursorPos(x1, y1);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x1, y1, 0, 0);
+                Thread.Sleep(militime);
+                SetCursorPos(x2, y2);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x2, y2, 0, 0);
+                counter++;
+                label1.Text = counter.ToString();
+                Thread.Sleep(militime);
+            }
+        }
+ 
+        public AC130()
+        {
+            InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_HOTKEY)
+            {
+                try
+                {
+                    militime = int.Parse(textBox1.Text);
+                }
+                catch
+                {
+                    militime = 500;
+                }
+                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+                if (key == Keys.F1)
+                {
+                    if (!AutoClick.IsAlive)
+                    {
+                        AutoClick2.Abort();
+                        AutoClick2 = new Thread(AClick2);
+                        AutoClick.Start();
+                        lstate.Text = "On";
+                        lstate.ForeColor = System.Drawing.Color.Green;
+                        Controls.Add(lstate);
+                    }
+                    else
+                    {
+                        AutoClick.Abort();
+                        AutoClick = new Thread(AClick);
+                        lstate.Text = "Off";
+                        lstate.ForeColor = System.Drawing.Color.Red;
+                        Controls.Add(lstate);
+                    }
+                } 
+                if (key == Keys.F5)
+                {
+                    if (!AutoClick2.IsAlive)
+                    {
+                        AutoClick.Abort();
+                        AutoClick = new Thread(AClick);
+                        AutoClick2.Start();
+                        lstate.Text = "On";
+                        lstate.ForeColor = System.Drawing.Color.Green;
+                        Controls.Add(lstate);
+                    }
+                    else
+                    {
+                        AutoClick2.Abort();
+                        AutoClick2 = new Thread(AClick2);
+                        lstate.Text = "Off";
+                        lstate.ForeColor = System.Drawing.Color.Red;
+                        Controls.Add(lstate);
+                    }
+                }
+                if (key == Keys.F6)
+                {
+                    AutoClick2.Abort();
+                    AutoClick2 = new Thread(AClick2);
+                    lstate.Text = "Off";
+                    lstate.ForeColor = System.Drawing.Color.Red;
+                    x1 = Cursor.Position.X;
+                    y1 = Cursor.Position.Y;
+                    label2.Text = String.Format("{0}, {1}", x1, y1);
+                }
+                if (key == Keys.F7)
+                {
+                    AutoClick2.Abort();
+                    AutoClick2 = new Thread(AClick2);
+                    lstate.Text = "Off";
+                    lstate.ForeColor = System.Drawing.Color.Red;
+                    x2 = Cursor.Position.X;
+                    y2 = Cursor.Position.Y;
+                    label3.Text = String.Format("{0}, {1}", x2, y2);
+                }
+            }
+        }
+        private void AC130_Load(object sender, EventArgs e)
+        {
+            AutoClick = new Thread(AClick);
+            AutoClick2 = new Thread(AClick2);
+            RegisterHotKey(this.Handle, (int)Keys.F1, 0, (uint)Keys.F1);
+            RegisterHotKey(this.Handle, (int)Keys.F5, 0, (uint)Keys.F5);
+            RegisterHotKey(this.Handle, (int)Keys.F6, 0, (uint)Keys.F6);
+            RegisterHotKey(this.Handle, (int)Keys.F7, 0, (uint)Keys.F7);
+            lstate.Text = "Off";
+            lstate.ForeColor = System.Drawing.Color.Red;
+            Controls.Add(lstate);
+            AutoClick.IsBackground = true;
+        }
+    }
+}
